6fb977a9763c6083518955a7f84df35ebfb6024e
houjinghp <784210432@qq.com>
2024-05-23 21:29:11 +0800

    hj: load data

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
+import numpy as np
+
+
 def main():
+    path = "./data/raw/PL004_event_ddp_expert_event_20230509-162021_0.bag.2ba4e5d23f5007cc82f234b8f0fc1061.npz"
+    data = np.load(path, allow_pickle=False)
+
     print("Finished...")
 
 
