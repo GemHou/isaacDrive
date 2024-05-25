1f43aece7223820c0ddfa9a5107fcaadd101a218
GemHou <784210432@qq.com>
2024-05-25 11:50:02 +0800

    hj: test device

--- main_test.py
+++ main_test.py
@@ -2,11 +2,13 @@ import torch
 
 from utils_agent import Agent
 
+DEVICE = torch.device("cpu")  # cuda:0 cpu
+
 
 def main():
     agent = Agent()
 
-    state_dict = torch.load("./data/interim/state_dict_temp.pt", map_location=torch.device("cuda:0"))
+    state_dict = torch.load("./data/interim/state_dict_temp.pt", map_location=DEVICE)
     agent.load_state_dict(state_dict)
 
     print("Finished")
