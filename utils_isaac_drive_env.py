6d919e1fcc88403f6196ee31612992ac96b6bdc6
GemHou <784210432@qq.com>
2024-05-25 23:17:11 +0800

    hj: other his

--- utils_isaac_drive_env.py
+++ utils_isaac_drive_env.py
@@ -165,10 +165,16 @@ class IsaacDriveEnv:
 
     def render(self):
         plt.cla()
-        tensor_oneTime_other_pos_start = self.tensor_batch_vectornet_object_feature[0, self.timestep, 1:, 0,
-                                         0:2]  # [99, 2]
-        tensor_cpu_oneTime_other_pos_start = tensor_oneTime_other_pos_start.cpu()
+
+        tensor_oneTime_other_pos_start = self.tensor_batch_vectornet_object_feature[0, self.timestep, 1:, 0, 0:2]
+        tensor_cpu_oneTime_other_pos_start = tensor_oneTime_other_pos_start.cpu()  # [99, 2]
         plt.scatter(tensor_cpu_oneTime_other_pos_start[:, 0], tensor_cpu_oneTime_other_pos_start[:, 1])
+
+        tensor_oneTime_other_pos_his_start = self.tensor_batch_vectornet_object_feature[0, self.timestep, 1:, :10, 0:2]
+        tensor_cpu_oneTime_other_pos_his_start = tensor_oneTime_other_pos_his_start.cpu()  # [99, 10, 2]
+        tensor_cpu_oneTime_other_pos_his_start = tensor_cpu_oneTime_other_pos_his_start.reshape(990, 2)
+        plt.scatter(tensor_cpu_oneTime_other_pos_his_start[:, 0], tensor_cpu_oneTime_other_pos_his_start[:, 1], alpha=0.1)
+
         plt.xlim(-50, 50)
         plt.ylim(-50, 50)
         numpy_oneTime_action_xy = self.tensor_batch_oneTime_action_xy[0].cpu().detach().numpy()
