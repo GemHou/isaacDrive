8e6d2dbe2f1d54527d682ea2a077f5e44bd4398b
GemHou <784210432@qq.com>
2024-06-08 16:24:58 +0800

    hj: ppo

--- ppo.py
+++ ppo.py
@@ -3,7 +3,7 @@ import torch
 from torch.optim import Adam
 import gym
 import time
-import vpg_core as core
+import ppo_core as core
 
 
 # from spinup.utils.logx import EpochLogger
@@ -11,9 +11,9 @@ import vpg_core as core
 # from spinup.utils.mpi_tools import mpi_fork, mpi_avg, proc_id, mpi_statistics_scalar, num_procs
 
 
-class VPGBuffer:
+class PPOBuffer:
     """
-    A buffer for storing trajectories experienced by a VPG agent interacting
+    A buffer for storing trajectories experienced by a PPO agent interacting
     with the environment, and using Generalized Advantage Estimation (GAE-Lambda)
     for calculating the advantages of state-action pairs.
     """
@@ -86,14 +86,14 @@ class VPGBuffer:
         return {k: torch.as_tensor(v, dtype=torch.float32) for k, v in data.items()}
 
 
-def vpg(env_fn, actor_critic=core.MLPActorCritic, ac_kwargs=dict(), seed=0,
-        steps_per_epoch=4000, epochs=50, gamma=0.99, pi_lr=3e-4,
-        vf_lr=1e-3, train_v_iters=80, lam=0.97, max_ep_len=1000,
-        logger_kwargs=dict(), save_freq=10):
+def ppo(env_fn, actor_critic=core.MLPActorCritic, ac_kwargs=dict(), seed=0,
+        steps_per_epoch=4000, epochs=50, gamma=0.99, clip_ratio=0.2, pi_lr=3e-4,
+        vf_lr=1e-3, train_pi_iters=80, train_v_iters=80, lam=0.97, max_ep_len=1000,
+        target_kl=0.01, logger_kwargs=dict(), save_freq=10):
     """
-    Vanilla Policy Gradient 
+    Proximal Policy Optimization (by clipping), 
 
-    (with GAE-Lambda for advantage estimation)
+    with early stopping based on approximate KL
 
     Args:
         env_fn : A function which creates a copy of the environment.
@@ -145,8 +145,9 @@ def vpg(env_fn, actor_critic=core.MLPActorCritic, ac_kwargs=dict(), seed=0,
                                            | make sure to flatten this!)
             ===========  ================  ======================================
 
+
         ac_kwargs (dict): Any kwargs appropriate for the ActorCritic object 
-            you provided to VPG.
+            you provided to PPO.
 
         seed (int): Seed for random number generators.
 
@@ -158,9 +159,20 @@ def vpg(env_fn, actor_critic=core.MLPActorCritic, ac_kwargs=dict(), seed=0,
 
         gamma (float): Discount factor. (Always between 0 and 1.)
 
+        clip_ratio (float): Hyperparameter for clipping in the policy objective.
+            Roughly: how far can the new policy go from the old policy while 
+            still profiting (improving the objective function)? The new policy 
+            can still go farther than the clip_ratio says, but it doesn't help
+            on the objective anymore. (Usually small, 0.1 to 0.3.) Typically
+            denoted by :math:`\epsilon`. 
+
         pi_lr (float): Learning rate for policy optimizer.
 
         vf_lr (float): Learning rate for value function optimizer.
+
+        train_pi_iters (int): Maximum number of gradient descent steps to take 
+            on policy loss per epoch. (Early stopping may cause optimizer
+            to take fewer than this.)
 
         train_v_iters (int): Number of gradient descent steps to take on 
             value function per epoch.
@@ -169,6 +181,10 @@ def vpg(env_fn, actor_critic=core.MLPActorCritic, ac_kwargs=dict(), seed=0,
             close to 1.)
 
         max_ep_len (int): Maximum length of trajectory / episode / rollout.
+
+        target_kl (float): Roughly what KL divergence we think is appropriate
+            between new and old policies after an update. This will get used 
+            for early stopping. (Usually small, 0.01 or 0.05.)
 
         logger_kwargs (dict): Keyword args for EpochLogger.
 
@@ -206,20 +222,24 @@ def vpg(env_fn, actor_critic=core.MLPActorCritic, ac_kwargs=dict(), seed=0,
 
     # Set up experience buffer
     local_steps_per_epoch = int(steps_per_epoch)  # / num_procs()
-    buf = VPGBuffer(obs_dim, act_dim, local_steps_per_epoch, gamma, lam)
+    buf = PPOBuffer(obs_dim, act_dim, local_steps_per_epoch, gamma, lam)
 
-    # Set up function for computing VPG policy loss
+    # Set up function for computing PPO policy loss
     def compute_loss_pi(data):
         obs, act, adv, logp_old = data['obs'], data['act'], data['adv'], data['logp']
 
         # Policy loss
         pi, logp = ac.pi(obs, act)
-        loss_pi = -(logp * adv).mean()
+        ratio = torch.exp(logp - logp_old)
+        clip_adv = torch.clamp(ratio, 1 - clip_ratio, 1 + clip_ratio) * adv
+        loss_pi = -(torch.min(ratio * adv, clip_adv)).mean()
 
         # Useful extra info
         approx_kl = (logp_old - logp).mean().item()
         ent = pi.entropy().mean().item()
-        pi_info = dict(kl=approx_kl, ent=ent)
+        clipped = ratio.gt(1 + clip_ratio) | ratio.lt(1 - clip_ratio)
+        clipfrac = torch.as_tensor(clipped, dtype=torch.float32).mean().item()
+        pi_info = dict(kl=approx_kl, ent=ent, cf=clipfrac)
 
         return loss_pi, pi_info
 
@@ -238,17 +258,24 @@ def vpg(env_fn, actor_critic=core.MLPActorCritic, ac_kwargs=dict(), seed=0,
     def update():
         data = buf.get()
 
-        # Get loss and info values before update
         pi_l_old, pi_info_old = compute_loss_pi(data)
         pi_l_old = pi_l_old.item()
         v_l_old = compute_loss_v(data).item()
 
-        # Train policy with a single step of gradient descent
-        pi_optimizer.zero_grad()
-        loss_pi, pi_info = compute_loss_pi(data)
-        loss_pi.backward()
-        # mpi_avg_grads(ac.pi)    # average grads across MPI processes
-        pi_optimizer.step()
+        # Train policy with multiple steps of gradient descent
+        for i in range(train_pi_iters):
+            pi_optimizer.zero_grad()
+            loss_pi, pi_info = compute_loss_pi(data)
+            # kl = mpi_avg(pi_info['kl'])
+            kl = pi_info['kl']
+            if kl > 1.5 * target_kl:
+                # logger.log('Early stopping at step %d due to reaching max kl.'%i)
+                break
+            loss_pi.backward()
+            # mpi_avg_grads(ac.pi)    # average grads across MPI processes
+            pi_optimizer.step()
+
+        # logger.store(StopIter=i)
 
         # Value function learning
         for i in range(train_v_iters):
@@ -259,9 +286,9 @@ def vpg(env_fn, actor_critic=core.MLPActorCritic, ac_kwargs=dict(), seed=0,
             vf_optimizer.step()
 
         # Log changes from update
-        kl, ent = pi_info['kl'], pi_info_old['ent']
+        kl, ent, cf = pi_info['kl'], pi_info_old['ent'], pi_info['cf']
         # logger.store(LossPi=pi_l_old, LossV=v_l_old,
-        #              KL=kl, Entropy=ent,
+        #              KL=kl, Entropy=ent, ClipFrac=cf,
         #              DeltaLossPi=(loss_pi.item() - pi_l_old),
         #              DeltaLossV=(loss_v.item() - v_l_old))
 
@@ -310,7 +337,7 @@ def vpg(env_fn, actor_critic=core.MLPActorCritic, ac_kwargs=dict(), seed=0,
             # logger.save_state({'env': env}, None)
             pass
 
-        # Perform VPG update!
+        # Perform PPO update!
         update()
 
         # Log info about epoch
@@ -325,6 +352,8 @@ def vpg(env_fn, actor_critic=core.MLPActorCritic, ac_kwargs=dict(), seed=0,
         # logger.log_tabular('DeltaLossV', average_only=True)
         # logger.log_tabular('Entropy', average_only=True)
         # logger.log_tabular('KL', average_only=True)
+        # logger.log_tabular('ClipFrac', average_only=True)
+        # logger.log_tabular('StopIter', average_only=True)
         # logger.log_tabular('Time', time.time()-start_time)
         # logger.dump_tabular()
 
@@ -333,15 +362,15 @@ if __name__ == '__main__':
     import argparse
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--env', type=str, default='CartPole-v0')
+    parser.add_argument('--env', type=str, default='Pendulum-v1')  # Pendulum-v1
     parser.add_argument('--hid', type=int, default=64)
     parser.add_argument('--l', type=int, default=2)
     parser.add_argument('--gamma', type=float, default=0.99)
     parser.add_argument('--seed', '-s', type=int, default=0)
     parser.add_argument('--cpu', type=int, default=4)
     parser.add_argument('--steps', type=int, default=4000)
-    parser.add_argument('--epochs', type=int, default=50)
-    parser.add_argument('--exp_name', type=str, default='vpg')
+    parser.add_argument('--epochs', type=int, default=500)
+    parser.add_argument('--exp_name', type=str, default='ppo')
     args = parser.parse_args()
 
     # mpi_fork(args.cpu)  # run parallel code with mpi
@@ -349,6 +378,8 @@ if __name__ == '__main__':
     # from spinup.utils.run_utils import setup_logger_kwargs
     # logger_kwargs = setup_logger_kwargs(args.exp_name, args.seed)
 
-    vpg(lambda: gym.make(args.env), actor_critic=core.MLPActorCritic,
+    ppo(lambda: gym.make(args.env), actor_critic=core.MLPActorCritic,
         ac_kwargs=dict(hidden_sizes=[args.hid] * args.l), gamma=args.gamma,
-        seed=args.seed, steps_per_epoch=args.steps, epochs=args.epochs)
+        seed=args.seed, steps_per_epoch=args.steps, epochs=args.epochs,
+        # logger_kwargs=logger_kwargs
+        )
