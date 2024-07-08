a44ad28203fd35fe2698356902c2e3bedfffac8d
GemHou <784210432@qq.com>
2024-07-08 15:48:36 +0800

    hj: std

--- main_test_throught.py
+++ main_test_throught.py
@@ -1,6 +1,7 @@
 import time
 import tqdm
 import torch
+import numpy as np
 
 from utils_agent import Agent, AgentAcceleration, AgentVehicleDynamic
 from utils_isaac_drive_env import IsaacDriveEnv
@@ -25,6 +26,7 @@ def prepare_agent():
 def sim_one_epoch(isaac_drive_env):
     tensor_batch_obs = isaac_drive_env.reset(batch_num=BATCH_NUM, data_mode=TRAIN_TEST_MODE)  # Train Test
     list_tensor_time_reward = []
+    start_time = time.time()
     while True:
         # tensor_batch_oneTime_action_xy = torch.randn(BATCH_NUM, 2, device=DEVICE)  # [B, 2]
         # tensor_batch_oneTime_action_xy = torch.zeros(BATCH_NUM, 2, device=DEVICE)  # [B, 2]
@@ -36,9 +38,12 @@ def sim_one_epoch(isaac_drive_env):
             isaac_drive_env.render()
         if done:
             break
+    throughout = BATCH_NUM * 254 / (time.time() - start_time)
+    print("")
     tensor_epoch_reward = torch.stack(list_tensor_time_reward)
     reward_per_step = torch.mean(tensor_epoch_reward)
     # print("reward_per_step: ", reward_per_step)
+    return throughout
 
 
 def main():
@@ -48,16 +53,16 @@ def main():
     # prepare agent
     # agent = prepare_agent()
 
-    start_time = time.time()
+    list_throughout = []
+
     for _ in tqdm.tqdm(range(50)):
         state_dict = torch.load("data/interim/state_dict_grad.pt", map_location=DEVICE)
         # agent.load_state_dict(state_dict)
-        sim_one_epoch(isaac_drive_env)
+        throughout = sim_one_epoch(isaac_drive_env)
+        list_throughout.append(throughout)
 
-    simulation_time = time.time() - start_time
-    print("simulation time: ", simulation_time)
-    throught = BATCH_NUM * 254 / simulation_time
-    print("throught: ", throught)
+    print("np.mean(list_throughout): ", np.mean(list_throughout))
+    print("np.std(list_throughout): ", np.std(list_throughout))
 
     print("Finished")
 
