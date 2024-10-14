83a69bb0fa400605ce129dfd9a60669e5d71e313
gemhou <1911061@tongji.edu.cn>
2024-10-14 22:23:40 +0800

    hj: average

--- main_plot_throught.py
+++ main_plot_throught.py
@@ -5,8 +5,8 @@ import numpy as np
 data1 = [1948, 3144, 5735, 9775, 15618, 25654, 37850, 44685, 46356]
 data2 = [1027, 1888, 3845, 7501, 15006, 28304, 53108, 95927, 117172]
 
-data1 = [x/1000 for x in data1]
-data2 = [x/1000 for x in data2]
+data1 = [x/10000 for x in data1]
+data2 = [x/10000 for x in data2]
 
 # 创建一个索引数组，用于x轴的标签
 ind = np.arange(len(data1))  # 这个长度应该和数据的长度一致
@@ -24,7 +24,7 @@ rects1 = ax.bar(ind, data1, width, label='CPU Parallelism', color=[19/256, 0/256
 rects2 = ax.bar(ind + width, data2, width, label='GPU Parallelism', color=[131/256, 5/256, 24/256])
 
 # 添加一些文字标签和标题
-ax.set_ylabel(r'Throughput $( \times 10^3 )$')  # 使用LaTeX语法设置上标
+ax.set_ylabel(r'Throughput $( \times 10^4 )$')  # 使用LaTeX语法设置上标
 ax.set_xlabel('Batch size')
 # ax.set_title('Scores by category (log scale)')
 ax.set_xticks(ind + width / 2)
