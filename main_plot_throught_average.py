a703e232d0e713e20282feb8a9ad3a1cb5f0dade
gemhou <1911061@tongji.edu.cn>
2024-10-14 22:11:34 +0800

    hj: std

--- main_plot_throught_average.py
+++ main_plot_throught_average.py
@@ -4,9 +4,8 @@ import numpy as np
 # 假设这是你的两组数据
 data1 = [1948, 1572, 1433, 1221, 976, 801, 591, 349, 289]
 data2 = [1027, 944, 961, 937, 937, 884, 829, 749, 732]
-
-data1 = [x/100 for x in data1]
-data2 = [x/100 for x in data2]
+data1_std = [20.0, 17.0, 16.8, 17.0, 17.3, 23.5, 29.1, 12.9, 14.3]
+data2_std = [5.0, 68.0, 43.8, 32.1, 22.3, 48.0, 15.6, 36.4, 8.9]
 
 # 创建一个索引数组，用于x轴的标签
 ind = np.arange(len(data1))  # 这个长度应该和数据的长度一致
@@ -15,13 +14,15 @@ ind = np.arange(len(data1))  # 这个长度应该和数据的长度一致
 fig, ax = plt.subplots(figsize=(3*1.37, 3))
 
 # 绘制第一组数据的折线图
-ax.plot(ind, data1, label='CPU Parallelism', color=[19/256, 0/256, 116/256], marker='o')
+ax.plot(ind, data1, label='CPU Parallelism', color=[19/256, 0/256, 116/256])
 
 # 绘制第二组数据的折线图
-ax.plot(ind, data2, label='GPU Parallelism', color=[131/256, 5/256, 24/256], marker='o')
+ax.plot(ind, data2, label='GPU Parallelism', color=[131/256, 5/256, 24/256])
+
+# 根据std标准差绘制折线阴影
 
 # 添加一些文字标签和标题
-ax.set_ylabel(r'Throughput $( \times 10^2 )$')  # 使用LaTeX语法设置上标
+ax.set_ylabel(r'Throughput')  # 使用LaTeX语法设置上标
 ax.set_xlabel('Batch size')
 ax.set_xticks(ind)
 ax.set_xticklabels(('1', '2', '4', '8', '16', '32', '64', '128', '160'))
