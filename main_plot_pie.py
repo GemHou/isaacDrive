81acdcb0904e743b953b31f0a197ee23affa2883
gemhou <1911061@tongji.edu.cn>
2024-10-15 10:13:47 +0800

    hj: color

--- main_plot_pie.py
+++ main_plot_pie.py
@@ -6,12 +6,19 @@ data = [457, 1433, 433, 1075, 2297, 970, 434, 2418]
 # 创建一个标签列表，用于饼图的每个部分
 labels = ['Category 1', 'Category 2', 'Category 3', 'Category 4', 'Category 5', 'Category 6', 'Category 7', 'Category 8']
 
+# 定义两种颜色
+color1 = [19/256, 0/256, 116/256]
+color2 = [131/256, 5/256, 24/256]
+
+# 创建颜色列表，交替使用两种颜色
+colors = [color1 if i % 2 == 0 else color2 for i in range(len(data))]
+
 # 绘制饼图
-plt.figure(figsize=(8, 8))
-plt.pie(data, labels=labels, autopct='%1.1f%%', startangle=140)
+plt.figure(figsize=(3*1.37, 3))
+plt.pie(data, labels=labels, autopct='%1.1f%%', startangle=140, colors=colors)
 
 # 添加标题
-plt.title('Pie Chart Example')
+# plt.title('Pie Chart Example')
 
 plt.savefig('data/processed/plot_pie.svg', format='svg', bbox_inches='tight')
 
