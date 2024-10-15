a16065aedc5d586c6489889a5b10cf28186597e8
gemhou <1911061@tongji.edu.cn>
2024-10-15 11:04:00 +0800

    hj: radar 2

--- main_plot_radar.py
+++ main_plot_radar.py
@@ -14,16 +14,24 @@ data1 = np.concatenate((data1, [data1[0]]))
 data2 = np.concatenate((data2, [data2[0]]))
 angles += angles[:1]
 
+color1 = [19/256, 0/256, 116/256]
+color2 = [131/256, 5/256, 24/256]
+
 # 绘制雷达图
 fig, ax = plt.subplots(figsize=(3*1.37, 3), subplot_kw=dict(polar=True))
-ax.fill(angles, data1, color='red', alpha=0.25)
-ax.fill(angles, data2, color='blue', alpha=0.25)
-ax.plot(angles, data1, color='red', linewidth=2, linestyle='solid', label='MotionLM')
-ax.plot(angles, data2, color='blue', linewidth=2, linestyle='solid', label='SDRL(Ours)')
+ax.fill(angles, data1, color=color1, alpha=0.25)
+ax.fill(angles, data2, color=color2, alpha=0.25)
+ax.plot(angles, data1, color=color1, linewidth=2, linestyle='solid', label='MotionLM')
+ax.plot(angles, data2, color=color2, linewidth=2, linestyle='solid', label='SDRL(Ours)')
 
 # 添加标签
 ax.set_xticks(angles[:-1])
 ax.set_xticklabels(labels)
+
+# max_value = max(data1.max(), data2.max())
+# ticks = np.arange(0.2, 1.4, 0.3)  # 从0.2开始，每次增加0.2
+# ax.set_yticklabels([" ", "0.4", " ", "0.8", " ", "1.2", " "])
+ax.set_yticklabels(["0.2", " ", "0.6", " ", "1.0", " ", "1.4"])
 
 # 添加图例
 plt.legend(loc='upper right', bbox_to_anchor=(0.1, 0.1), frameon=False)
