741c6088e2940af150b25f829c002194d8cd3e7f
gemhou <1911061@tongji.edu.cn>
2024-10-15 17:34:33 +0800

    hj: data

--- main_plot_gen.py
+++ main_plot_gen.py
@@ -1,11 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-# 新的两组数据，百分比转换为小数形式
+# 原始数据
 data1 = np.array([18.60, 14.10, 13.50, 12.30, 11.60, 10.80, 9.80, 10.20, 10.30, 10.10])
-
-# 假设的标准差，这里需要您提供具体的数值，如果没有提供，我将使用假设值
-# data1_std = np.array([0.49, 0.65, 0.65, 1.01, 1.01, 0.44, 0.21*2, 0.15*2, 0.21*2, 0.21*2])
 
 # 创建一个索引数组，用于x轴的标签
 ind = np.arange(len(data1))  # 这个长度应该和数据的长度一致
@@ -13,11 +10,21 @@ ind = np.arange(len(data1))  # 这个长度应该和数据的长度一致
 # 创建一个图形框架
 fig, ax = plt.subplots(figsize=(3*1.37, 3))
 
-# 绘制第一组数据的折线图
-ax.plot(ind, data1, label='Vehicle SAIC LS6', color=[19/256, 0/256, 116/256], shape="o")
+# 绘制原始数据的折线图
+ax.plot(ind, data1, label='Vehicle SAIC LS6', color=[19/256, 0/256, 116/256], marker="o", alpha=0.5)
 
-# 根据std标准差绘制折线阴影
-# ax.fill_between(ind, data1 - data1_std * 1, data1 + data1_std * 1, color=[19/256, 0/256, 116/256], alpha=0.2)
+# 生成随机波动的数据
+np.random.seed(0)  # 设置随机种子以确保结果可复现
+data2 = data1 + np.random.normal(0, 0.5, len(data1)) + 0.1
+data3 = data1 + np.random.normal(0, 0.5, len(data1)) - 0.1
+data4 = data1 + np.random.normal(0, 0.5, len(data1)) + 0.5
+data5 = data1 + np.random.normal(0, 0.5, len(data1)) - 0.5
+
+# 绘制随机波动的数据
+ax.plot(ind, data2, label='Vehicle SAIC LS6 + Noise 1', marker="o", color=[131/256+0.2, 5/256, 24/256], alpha=0.5)
+ax.plot(ind, data3, label='Vehicle SAIC LS6 + Noise 2', marker="o", color=[19/256, 0/256+0.2, 116/256], alpha=0.5)
+ax.plot(ind, data4, label='Vehicle SAIC LS6 + Noise 3', marker="o", color=[131/256, 5/256, 24/256+0.2], alpha=0.5)
+ax.plot(ind, data5, label='Vehicle SAIC LS6 + Noise 4', marker="o", color=[19/256+0.2, 0/256+0.2, 116/256+0.2], alpha=0.5)
 
 # 添加一些文字标签和标题
 ax.set_ylabel('Collision rate (%)')  # 使用LaTeX语法设置上标
