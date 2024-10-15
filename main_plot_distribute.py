2c7e4ffa29511438d2be314c6d5fcda14ccd09e3
gemhou <1911061@tongji.edu.cn>
2024-10-15 16:09:15 +0800

    hj: axvline

--- main_plot_distribute.py
+++ main_plot_distribute.py
@@ -5,9 +5,9 @@ import numpy as np
 np.random.seed(0)
 
 # 生成两组数据，均值分别为1.2和1.0，标准差为0.2
-data1_part1 = np.random.normal(1.0, 0.2, 300)
-data1_part2 = np.random.normal(1.3, 0.3, 350)
-data1_part3 = np.random.normal(1.7, 0.2, 350)
+data1_part1 = np.random.normal(1.1, 0.2, 300)
+data1_part2 = np.random.normal(1.4, 0.3, 350)
+data1_part3 = np.random.normal(0.8, 0.2, 350)
 data1 = np.concatenate((data1_part1, data1_part2, data1_part3))
 
 data2_part1 = np.random.normal(1.2, 0.2, 300)
@@ -33,17 +33,22 @@ width = 0.08
 fig, ax = plt.subplots(figsize=(3*1.37, 3))
 
 # 绘制第一组数据的柱状图
-rects1 = ax.bar(ind , hist1, width, label='MotionLM', color=[19/256, 0/256, 116/256], alpha=0.5)
+rects1 = ax.bar(ind, hist1, width, label='MotionLM', color=[19/256, 0/256, 116/256], alpha=0.5)
 
-# 绘制第二组数据的柱状图，位置稍微偏移
-rects2 = ax.bar(ind , hist2, width, label='SDRL(Ours)', color=[131/256, 5/256, 24/256], alpha=0.5)
+# 绘制第二组数据的柱状图
+rects2 = ax.bar(ind, hist2, width, label='SDRL(Ours)', color=[131/256, 5/256, 24/256], alpha=0.5)
+
+# 计算均值
+mean1 = np.mean(data1)
+mean2 = np.mean(data2)
+
+# 添加均值的虚线
+ax.axvline(x=mean1, color=[19/256, 0/256, 116/256], linestyle='--')  # , label=f'Mean of MotionLM: {mean1:.2f}'
+ax.axvline(x=mean2, color=[131/256, 5/256, 24/256], linestyle='--')  # , label=f'Mean of SDRL(Ours): {mean2:.2f}'
 
 # 添加一些文字标签和标题
 ax.set_ylabel('Probability Density')
 ax.set_xlabel('Time to Collision (s)')
-# ax.set_xlim(0, 2.5)  # 设置x轴的范围为0到2.5
-# ax.set_xticks(ind)  # 设置x轴的刻度为每个类别的中心点
-# ax.set_xticklabels([f'{i:.1f}' for i in ind], fontsize=8)  # 设置x轴的刻度标签
 ax.legend(frameon=False)
 
 # 保存图形
