9f0308526d0ac5eae083901d1354001afc7222db
gemhou <1911061@tongji.edu.cn>
2024-10-15 12:20:46 +0800

    hj: radar

--- main_plot_radar.py
+++ main_plot_radar.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # 准备数据
-labels = np.array(['Safety', 'Panic', 'Comfort', 'Speed', 'Energy', 'Rule'])
+labels = np.array(['  Safety', 'Panic', 'Comfort', 'Speed  ', 'Energy', 'Rule'])
 data1 = np.array([0.8, 0.7, 0.8, 0.9, 1.0, 1.0])
 data2 = np.array([1.4, 1.5, 1.2, 1.1, 0.95, 0.95])
 
@@ -22,19 +22,23 @@ fig, ax = plt.subplots(figsize=(3*1.37, 3), subplot_kw=dict(polar=True))
 ax.fill(angles, data1, color=color1, alpha=0.25)
 ax.fill(angles, data2, color=color2, alpha=0.25)
 ax.plot(angles, data1, color=color1, linewidth=2, linestyle='solid', label='MotionLM')
-ax.plot(angles, data2, color=color2, linewidth=2, linestyle='solid', label='SDRL(Ours)')
+ax.plot(angles, data2, color=color2, linewidth=2, linestyle='solid', label='SDRL')
 
 # 添加标签
 ax.set_xticks(angles[:-1])
 ax.set_xticklabels(labels)
 
-# max_value = max(data1.max(), data2.max())
-# ticks = np.arange(0.2, 1.4, 0.3)  # 从0.2开始，每次增加0.2
-# ax.set_yticklabels([" ", "0.4", " ", "0.8", " ", "1.2", " "])
+# 设置数值横坐标
+# ax.set_yticks([0.2, 0.6, 1.0, 1.4])  # 设置数值横坐标
 ax.set_yticklabels(["0.2", " ", "0.6", " ", "1.0", " ", "1.4"])
 
 # 添加图例
-plt.legend(loc='upper right', bbox_to_anchor=(0.1, 0.1), frameon=False)
+plt.legend(
+    # loc='lower right',
+    bbox_to_anchor=(1.3, 0.2),
+    frameon=False,
+    fontsize=8,
+)
 
 plt.savefig('data/processed/plot_radar.svg', format='svg', bbox_inches='tight')
 
